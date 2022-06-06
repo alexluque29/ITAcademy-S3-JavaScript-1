77872c09a7de6e2dce635d2582f3b7dac35ddc3d
Alex Luque <alexluque29@gmail.com>
2022-06-06 21:31:26 +0200

    Ejercicio-4

--- js/shop.js
+++ js/shop.js
@@ -77,7 +77,7 @@ var total = 0;
 function buy(id) {
   // 1. Loop for to the array products to get the item to add to cart
   // 2. Add found product to the cartList array
-  let contador, i;
+  let contador, i;  
   for (i = 0; i < products.length; i++) {
         if (products[i].id == id) {
         cartList.push(products[i]);
@@ -99,12 +99,26 @@ function calculateTotal() {
     for (i=0; i<cartList.length; i++){
         total += cartList[i].price;
     }
+    console.log(total);
 }
 
 // Exercise 4
 function generateCart() {
-    // Using the "cartlist" array that contains all the items in the shopping cart, 
-    // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
+  // Using the "cartlist" array that contains all the items in the shopping cart,
+  // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
+  let i, checkProduct;
+  for (i = 0; i < cartList.length; i++) {
+    checkProduct = cartList[i];
+
+    if (!cart.includes(checkProduct)) {
+      checkProduct.quantity = 1;
+      cart.push(checkProduct);
+    } 
+    else if (cart.includes(checkProduct)) {
+      checkProduct.quantity++;
+    }
+  }
+  console.log(cart);
 }
 
 // Exercise 5
